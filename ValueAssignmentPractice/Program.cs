26eca44ff91f24583eb60d864b749adaebbd6dd6
Bryce <ImperialEagle1998@gmail.com>
2020-09-21 23:19:00 -0500

    New Addition arithmetic statement added using variables c and d.

--- ValueAssignmentPractice/Program.cs
+++ ValueAssignmentPractice/Program.cs
@@ -17,7 +17,21 @@ namespace ValueAssignmentPractice
             Console.Write("\n\nAdd and Assign (and other operations): ");
             Console.Write("\t a += b (8 += 4 ) \t a = " + (a += b) + " Now let's try multiplication: \n");
             Console.Write("\t a *= b ( 8 *= 4 ) \t a = " + (a *= b) + "Ok, let's try some division: \n" );
-            Console.Write("\t a /= b (8 /= 4) \t a =  " + (a /= b) + "Ok, welp, Multiplication and Divison have now been taken care of.");
+            Console.Write("\t a /= b (8 /= 4) \t a =  " + (a /= b) + "Ok, welp, Multiplication and Divison have now been taken care of. \n" );
+
+
+
+
+            int c;
+            int d;
+
+
+            Console.Write("\t c = " + (c = 20));
+            Console.Write("\t d = " + (d = 10));
+
+            Console.Write("\t c += d (20 += 10 ) is equal to:" + (c += d));
+
+
 
 
 
